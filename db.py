280b64c309efef8d76610735f808ec2cf91d6f1b
TonySD <asvagin_2@edu.hse.ru>
2024-05-08 00:31:21 +0300

    Impl: structure for DB class

---
+++ db.py
@@ -0,0 +1,28 @@
+import psycopg2
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+class DB:
+    def __init__(self):
+        ...
+
+    def open_connection():
+        ...
+
+    def execute_command():
+        ...
+
+    def select_emails():
+        ...
+
+    def select_phones():
+        ...
+
+    def insert_phones():
+        ...
+
+    def insert_emails():
+        ...
+
+db = DB()
