59d7e2aa3efcd52c80b4acc3a745be1765668d21
TonySD <ahtoxa.b@gmail.com>
2024-05-13 16:22:42 +0300

    Impl: Created sql scripts for db docker

---
+++ db/sql/create_tables.sql
@@ -0,0 +1,9 @@
+CREATE TABLE phone_numbers (
+    id SERIAL PRIMARY KEY,
+    phone VARCHAR(16)
+);
+
+CREATE TABLE emails (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(128)
+);
---
+++ db/sql/insert_test_data.sql
@@ -0,0 +1,9 @@
+INSERT INTO phone_numbers (phone) VALUES
+('89065119192'),
+('83476378633'),
+('89161606677');
+
+INSERT INTO emails (email) VALUES
+('aaa@bbb.ccc'),
+('ahtoxa.b@gmail.com'),
+('hehehe@hihihi.ru');
