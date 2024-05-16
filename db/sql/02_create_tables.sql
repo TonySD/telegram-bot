752e3cf70c0c3e301f73699a4b113a45d9ce1625
TonySD <ahtoxa.b@gmail.com>
2024-05-16 13:40:08 +0300

    UPD: ansible init commit

--- db/sql/02_create_tables.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE phone_numbers (
-    id SERIAL PRIMARY KEY,
-    phone VARCHAR(16)
-);
-
-CREATE TABLE emails (
-    id SERIAL PRIMARY KEY,
-    email VARCHAR(128)
-);
--- db/sql/03_insert_test_data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO phone_numbers (phone) VALUES
-('89065119192'),
-('83476378633'),
-('89161606677');
-
-INSERT INTO emails (email) VALUES
-('aaa@bbb.ccc'),
-('ahtoxa.b@gmail.com'),
-('hehehe@hihihi.ru');
