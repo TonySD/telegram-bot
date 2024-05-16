07906203103472a45495381f889ac3b5816badae
TonySD <ahtoxa.b@gmail.com>
2024-05-16 12:24:57 +0300

    Feat: fixes to move .env in docker-compose directory

--- db/entrypoint.sh
+++ db/entrypoint.sh
@@ -3,9 +3,9 @@ service postgresql start
 
 initdb
 pg_ctl start
-echo "host replication repl_user 172.30.1.0/16 trust" >> /var/lib/postgresql/data/pg_hba.conf 
-echo "host all all 172.30.1.0/16 trust" >> /var/lib/postgresql/data/pg_hba.conf 
-psql -U postgres -c "CREATE database bot_db;" 
+echo "host replication $DB_REPL_USER 172.30.0.0/24 trust" >> /var/lib/postgresql/data/pg_hba.conf 
+echo "host all all 172.30.0.0/24 trust" >> /var/lib/postgresql/data/pg_hba.conf 
+psql -U postgres -c "CREATE database $DB_DATABASE;" 
 psql -U postgres -d bot_db -f /docker-entrypoint-initdb.d/01_create_repl_user.sql
 psql -U postgres -d bot_db -f /docker-entrypoint-initdb.d/02_create_tables.sql
 psql -U postgres -d bot_db -f /docker-entrypoint-initdb.d/03_insert_test_data.sql
--- db_repl/init.sh
+++ db_repl/init.sh
@@ -3,7 +3,7 @@
 pg_ctl stop -D /var/lib/postgresql/data
 rm -rf /var/lib/postgresql/data/*
 
-until pg_basebackup -R -h db -U repl_user -D /var/lib/postgresql/data -P; do
+until pg_basebackup -R -h $DB_HOST -U $DB_REPL_USER -D /var/lib/postgresql/data -P; do
     echo 'Master server is not launched yet...'
     sleep 5s
 done
